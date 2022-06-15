b9b33d248ab6ed043e92d98948860b4f7dfc85b2
Bluefalcon04v <101953634+Bluefalcon04v@users.noreply.github.com>
2022-06-15 22:41:49 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+// nav bar active page
+
+const activepage = window.location.pathname;
+const navlinks = document.querySelectorAll('nav ul div a').
+forEach(link =>{
+    if(link.href.includes(`${activepage}`)){
+        link.classList.add(`active`);
+    }
+});
+
+// dropdown menu 
+
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown =>{
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', ()=>{
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+    options.forEach(option=>{
+        option.addEventListener('click', ()=>{
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            options.forEach(option=>{
+                option.classList.remove('active');
+            });
+            // option.classList.add('active');
+            option.classList.add('active');
+        });
+    });
+});
+
